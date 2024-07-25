f0635d5d071dbbecf3e70e1372d140c9893757db
iasmin <iasmin.ufpe@gmail.com>
2024-07-24 21:29:54 -0300

    init: add client socket to establish connection

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
-#Aqui vocês irão colocar seu algoritmo de aprendizado
+import connection as cn
+
+
+socket = cn.connect(111)
+
+while True:
+    state, reward = cn.get_state_reward(socket, "jump")
+    print(state, reward)
